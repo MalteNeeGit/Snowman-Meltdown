fc79b8d794087056b2b02d43d82808e4b345188e
Malte Neemann <malteneemann129@gmail.com>
2026-02-13 14:37:16 +0100

    We made the game more stable and added a funktion for the input validation, to check if user input is valid. We optimized the look of the game by adding emojis and colors. And we added a replay option to improve user experience.

--- game_logic.py
+++ game_logic.py
@@ -2,6 +2,12 @@ import ascii_art
 import snowman
 import random
 
+#ANSI COLORS:
+GREEN = "\033[0;32m"
+CYAN = "\033[0;36m"
+LIGHT_RED = "\033[1;31m"
+END = "\033[0m"
+
 def get_random_word():
     """Selects a random word from the list."""
     return snowman.WORDS[random.randint(0, len(snowman.WORDS) - 1)]
@@ -9,6 +15,7 @@ def get_random_word():
 
 def display_game_state(mistakes, secret_word, guessed_letters):
     #Display the snowman for the current number of mistakes.
+    print("\n" + "❄️" * 15)
     print(ascii_art.STAGES[mistakes])
     #build a display version of the secret word
     display_word = ""
@@ -18,7 +25,22 @@ def display_game_state(mistakes, secret_word, guessed_letters):
         else:
             display_word += "_ "
     print("Word: ", display_word)
-    print("\n")
+    print()
+    print("❄️" * 15 + "\n")
+
+def input_validation(guessed_letters):
+    while True:
+        user_input = input(f"{CYAN}Guess a letter: {END}").lower().strip()
+
+        if len(user_input) != 1:
+            print(f"{LIGHT_RED}Please enter a single letter{END}")
+        elif not user_input.isalpha():
+            print(f"{LIGHT_RED}We need letters, try any from A - Z{END}")
+        elif user_input in guessed_letters:
+            print(f"{LIGHT_RED}You already had this letter{END}")
+            continue
+        else:
+            return user_input
 
 
 def play_game():
@@ -27,23 +49,19 @@ def play_game():
     mistakes = 0
     max_mistakes = 3
 
-    print("Welcome to Snowman Meltdown!")
-    #For now, display the initial game state.
+    print(f"{CYAN}Welcome to Snowman Meltdown!{END}")
 
     while mistakes < max_mistakes:
         saved = True
         display_game_state(mistakes, secret_word, guessed_letters)
 
-        #Prompt user for one guess (logic to be enhanced later)
-        guess = input("Guess a letter: ").lower()
+        guess = input_validation(guessed_letters)
         print("You guessed:", guess)
 
-        if guess in guessed_letters:
-            print("You already had this letter")
-            continue
         if guess in secret_word:
             guessed_letters.append(guess)
         else:
+            print(f"{guess} {LIGHT_RED}is not in the word we are looking for{END}")
             mistakes += 1
 
         for char in secret_word:
@@ -55,12 +73,35 @@ def play_game():
 
     if mistakes < max_mistakes:
         print(ascii_art.STAGES[mistakes])
-        print("☃️ Yeahh, you saved the Snowman ☃️")
-        print(f"The word was {secret_word}")
+        print(f"☃️ {GREEN}Yeahh, you saved the Snowman{END} ☃️")
+        print(f"{GREEN}The word was:{END} {secret_word}")
+
     else:
         print(ascii_art.STAGES[mistakes])
-        print("Game Over! The Snowman melted :-(")
-        print(f"The word was: {secret_word}")
+        print(f"{LIGHT_RED}Game Over! The Snowman melted :-({END}")
+        print(f"{LIGHT_RED}The word was:{END} {secret_word}")
+
+def ask_to_play_again():
+    while True:
+        choice = input(f"\n{CYAN}Would you like to play again?{END} {GREEN}(y/n): {END}").lower().strip()
+        if choice == 'y':
+            return True
+        elif choice == 'n':
+            return False
+        else:
+            print(f"{CYAN}Please enter 'y' for Yes or 'n' for No. {END}")
 
 
-play_game()
+def main():
+    while True:
+        # Start first round:
+        play_game()
+
+        #if Game is finished, ask to play again
+        if not ask_to_play_again():
+            print(f"{GREEN}Thanks for playing Snowman Meltdown! Goodbye! ☃️👋{END}")
+            break
+
+
+if __name__ == "__main__":
+    main()
